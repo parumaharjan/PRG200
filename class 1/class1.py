4a084b3a72db4f04543ce0461486794c4689dbef
Paru Maharjan <parumaharjan098@gmail.com>
2026-07-08 08:49:42 +0545

    Week 1

---
+++ class 1/class1.py
@@ -0,0 +1,4 @@
+value = int(input ("enter a value "))
+a = int (input ("enter a value "))
+sum = a + value
+print (sum) 
--- class1.py
+++
@@ -1,5 +0,0 @@
-
-value = int(input ("enter a value"))
-a = int (input ("enter a value"))
-sum = a + value
-print (sum) 
